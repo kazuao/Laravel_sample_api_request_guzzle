004841445c890b871b3f56ea5319cec969640027
[STV]Kazunori Aoki <k.aoki@stv-tech.co.jp>
2020-11-20 10:45:12 +0900

    get, postのAPIリクエストサンプル実装

---
+++ request_api_guzzle/app/Http/Controllers/PostController.php
@@ -0,0 +1,65 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use GuzzleHttp\Client;
+
+class PostController extends Controller
+{
+    public function index()
+    {
+        $tag_id = "laravel";
+
+        $url = "https://qiita.com/api/v2/tags/" . $tag_id . "/items?page=1&per_page=20";
+        $method = "GET";
+
+        //接続
+        $client = new Client();
+
+        $response = $client->request($method, $url);
+
+        $posts = $response->getBody();
+        $posts = json_decode($posts, true);
+
+        return view('index', ['posts' => $posts]);
+    }
+
+    public function send(Request $request)
+    {
+        $url = 'https://qiita.com/api/v2/items';
+        $method = 'POST';
+        $token = '<取得したQuiitaのアクセストークン>';
+
+        $data = [
+            'title' => $request->title,
+            'body' => $request->body,
+            'private' => $request->private === 'private' ? true : false,
+            'tags' => [
+                [
+                    'name' => $request->tag,
+                ],
+            ],
+        ];
+
+        $client = new Client;
+
+        $options = [
+            'json' => $data,
+            'headers' => [
+                'Authorization' => 'Bearer ' . $token,
+                'Content-Type' => 'application/json',
+            ]
+        ];
+
+        $response = $client->request($method, $url, $options);
+
+        $post = $response->getBody();
+        $post = json_decode($post, true);
+
+        // レスポンスから新規記事のURLを取得
+        $new_post_url = $post['url'];
+
+        return rediredt('/create')->with('flash_message', '<a href=' . $new_post_url . '>記事</a>を投稿しました');
+    }
+}
--- request_api_guzzle/routes/web.php
+++ request_api_guzzle/routes/web.php
@@ -13,6 +13,9 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/', 'PostController@index');
+
+Route::get('/create', function() {
+    return view('form');
 });
+Route::post('/send', 'PostController@send');
